908407af1690134d622fc08885a2f049bdcc3dd2
Chase Manning <chase@manning.dev>
2024-05-26 17:22:30 +0100

    :rocket: add shard animations to inventory

--- src/pages/landing/ShardToCard.tsx
+++ src/pages/landing/ShardToCard.tsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styled from "styled-components";
 import shardIcon from "../../assets/logo/logo.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { selectShards, setShards } from "../../state/uiSlice";
+
+const SHARD_MOVE_TIME = 1000;
 
 const StyledShardToCard = styled.div`
   position: relative;
@@ -14,7 +18,7 @@ const Card = styled.div<{ $open: boolean }>`
   border-radius: 2rem;
 
   transform: scale(${({ $open }) => ($open ? 1 : 0)});
-  transition: transform 0.5s;
+  transition: transform 0.3s linear;
 `;
 
 const Header = styled.div`
@@ -44,9 +48,9 @@ const ShardContainer = styled.div`
 `;
 
 const Shard = styled.img`
-  height: 20rem;
+  height: 16rem;
   transform: translate(0, 0) scale(1);
-  transition: transform 2s;
+  transition: transform ${SHARD_MOVE_TIME}ms;
 `;
 
 interface Props {
@@ -57,7 +61,32 @@ interface Props {
 }
 
 const ShardToCard = ({ first, index, header, body }: Props) => {
+  const dispatch = useDispatch();
+  const shards = useSelector(selectShards);
+  const shardRef = useRef<HTMLImageElement>(null);
   const [open, setOpen] = useState(false);
+  const [transform, setTransform] = useState({ x: 0, y: 0 });
+  const [opacity, setOpacity] = useState(1);
+
+  const moveShard = () => {
+    if (!shardRef.current) return;
+    const shardBox = shardRef.current.getBoundingClientRect();
+    const startX = shardBox.x;
+    const startY = shardBox.y;
+    const endX = window.innerWidth - shardBox.width - 47;
+    const endY = 0 - 30;
+
+    setTransform({ x: endX - startX, y: endY - startY });
+
+    if (shards === null) {
+      dispatch(setShards(0));
+    }
+
+    setTimeout(() => {
+      setOpacity(0);
+      dispatch(setShards((shards || 0) + 1));
+    }, SHARD_MOVE_TIME);
+  };
 
   return (
     <StyledShardToCard>
@@ -66,7 +95,21 @@ const ShardToCard = ({ first, index, header, body }: Props) => {
         <Body>{body}</Body>
       </Card>
       <ShardContainer>
-        <Shard src={shardIcon} alt="shard" onClick={() => setOpen(true)} />
+        <Shard
+          ref={shardRef}
+          style={{
+            transform: `translate(${transform.x}px, ${transform.y}px) scale(${
+              open ? 0.15 : 1
+            })`,
+            opacity,
+          }}
+          src={shardIcon}
+          alt="shard"
+          onClick={() => {
+            setOpen(true);
+            moveShard();
+          }}
+        />
       </ShardContainer>
     </StyledShardToCard>
   );
